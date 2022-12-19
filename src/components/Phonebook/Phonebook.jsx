df5ee51bc7ad15f4b4848a74e2d4487753cae61c
Sergey Lisoviy <serlipaua@gmail.com>
2022-12-19 14:02:20 +0200

    start phonebook

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import Box from 'components/Box/Box';
 import { nanoid } from 'nanoid';
 // import { Input } from './Input/Input';
@@ -6,22 +6,12 @@ import { nanoid } from 'nanoid';
 import { Form } from 'components/Phonebook/Form/Form';
 import { Contacts } from 'components/Phonebook/Contacts/Contacts';
 
-export class Phonebook extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Clarence Mcculloch', number: '123-12-63' },
-      { id: 'id-2', name: 'Keiren Emery', number: '483-25-09' },
-      { id: 'id-3', name: 'Eliot Simpson', number: '321-67-12' },
-      { id: 'id-4', name: 'Alfie-Lee Whitmore', number: '542-52-22' },
-      { id: 'id-5', name: 'Teddy Parkes', number: '131-22-22' },
-      { id: 'id-6', name: 'Zachariah Brandt', number: '451-69-69' },
-      { id: 'id-7', name: 'Kacie Hume', number: '777-77-77' },
-    ],
-    filter: '',
-  };
+export const Phonebook = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setfilter] = useState('');
 
-  handleSubmitForm = ({ name, number }) => {
-    if (this.checkSameName(name)) {
+  const handleSubmitForm = ({ name, number }) => {
+    if (checkSameName(name)) {
       return alert(`${name} уже в списке контактов!`);
     }
 
@@ -32,11 +22,11 @@ export class Phonebook extends Component {
     });
   };
 
-  checkSameName = name => {
-    return this.state.contacts.find(el => el.name === name);
+  const checkSameName = name => {
+    return contacts.find(el => el.name === name);
   };
 
-  deleteContact = idContact => {
+  const deleteContact = idContact => {
     this.setState(prevState => {
       return {
         contacts: prevState.contacts.filter(
@@ -46,41 +36,39 @@ export class Phonebook extends Component {
     });
   };
 
-  filterChange = e => {
+  const filterChange = e => {
     const filterNormalize = e.currentTarget.value.toLowerCase();
     this.setState({ filter: filterNormalize });
   };
 
-  filtred = () => {
+  const filtred = () => {
     const { filter, contacts } = this.state;
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter)
     );
   };
 
-  render() {
-    const filtredContacts = this.filtred();
+  const filtredContacts = filtred();
 
-    return (
-      <Box
-        width="300px"
-        m="20px"
-        textAlign="center"
-        border="2px solid"
-        bc="black"
-        borderRadius="10px"
-        backgroundColor="#0caabf"
-        pb="20px"
-      >
-        <Form onSubmitForm={this.handleSubmitForm} />
-        {/* <Filter value={this.state.filter} onChange={this.filterChange} /> */}
-        <Contacts
-          contacts={filtredContacts}
-          onDeleteContact={this.deleteContact}
-          filterValue={this.state.filter}
-          filterOnChange={this.filterChange}
-        />
-      </Box>
-    );
-  }
-}
+  return (
+    <Box
+      width="300px"
+      m="20px"
+      textAlign="center"
+      border="2px solid"
+      bc="black"
+      borderRadius="10px"
+      backgroundColor="#0caabf"
+      pb="20px"
+    >
+      <Form onSubmitForm={handleSubmitForm} />
+      {/* <Filter value={this.state.filter} onChange={this.filterChange} /> */}
+      <Contacts
+        contacts={filtredContacts}
+        onDeleteContact={deleteContact}
+        filterValue={filter}
+        filterOnChange={filterChange}
+      />
+    </Box>
+  );
+};
